a996ea731fa95266e649731f8cd144ef6e054462
Yu Kang <kangy.me@gmail.com>
2025-07-31 14:17:59 +0000

    feat: tokenizer

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -1,146 +1,91 @@
-import os
-from typing import BinaryIO
+import json
 import regex as re
-from collections import Counter
-from tqdm import tqdm
-from concurrent.futures import ProcessPoolExecutor
+from collections.abc import Iterable, Iterator
 
+class Tokenizer:
+    def __init__(self, vocab: dict[int, bytes], merges: list[tuple[bytes, bytes]], special_tokens: list[str] | None = None):
+        self.vocab = vocab
+        self.merges = merges
+        self.special_tokens = special_tokens
+        if special_tokens:
+            for token in special_tokens:
+                b_token = token.encode("utf-8")
+                if b_token not in set(self.vocab.values()):
+                    self.vocab[len(self.vocab)] = b_token
+        self.vocab_inverse = {v:k for k, v in self.vocab.items()}
+        self.merges_ranks = {(start, end):i for i, (start, end) in enumerate(merges)}
+    
+    @classmethod
+    def from_files(cls, vocab_filepath: str, merges_filepath: str, special_tokens: list[str] | None = None):
+        with open(vocab_filepath) as f:
+            vocab = json.load(f)
+        merges = []
+        with open(merges_filepath) as f:
+            for line in f:
+                cleaned_line = line.rstrip()
+                if cleaned_line and len(cleaned_line.split(" ")) == 2:
+                    merges.append(tuple(line.split(" ")))
+        tokenizer = cls(vocab=vocab, merges=merges, special_tokens=special_tokens)
+        return tokenizer
+    
+    def encode(self, text: str) -> list[int]:
+        PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+        token_list = []
+        last_idx = 0
 
-def find_chunk_boundaries(file: BinaryIO, desired_num_chunks: int, split_special_token: bytes) -> list[int]:
-    """
-    Chunk the file into parts that can be counted independently.
-    May return fewer chunks if the boundaries end up overlapping.
-    """
-    assert isinstance(split_special_token, bytes), "Must represent special token as a bytestring"
+        if self.special_tokens:
+            special_tokens_sorted = sorted(self.special_tokens, key=len, reverse=True)
+            special_pattern = "|".join(map(re.escape, special_tokens_sorted))
+            for m in re.finditer(special_pattern, text, ):
+                split_chunk = text[last_idx:m.start()]
+                last_idx = m.end()
 
-    # Get total file size in bytes
-    file.seek(0, os.SEEK_END)
-    file_size = file.tell()
-    file.seek(0)
+                for word_match in re.finditer(PAT, split_chunk):
+                    word = word_match.group()
+                    bytes_tokens = [bytes([i]) for i in word.encode("utf-8")]
+                    
+                    while True:
+                        pairs = [(i, (bytes_tokens[i], bytes_tokens[i+1]))
+                                for i in range(len(bytes_tokens)-1)]
+                        min_pair = min(pairs, key=lambda x: self.merges_ranks.get(x[1], float("inf")), default=None)
+                        if not min_pair or self.merges_ranks.get(min_pair[1], float("inf")) == float("inf"):
+                            break
 
-    chunk_size = file_size // desired_num_chunks
+                        idx, (start, end) = min_pair
+                        bytes_tokens[idx] = start + end
+                        bytes_tokens.pop(idx+1)
 
-    # Initial guesses for chunk boundary locations, uniformly spaced
-    # Chunks start on previous index, don't include last index
-    chunk_boundaries = [i * chunk_size for i in range(desired_num_chunks + 1)]
-    chunk_boundaries[-1] = file_size
+                    token_list.extend([self.vocab_inverse[i] for i in bytes_tokens])
+                token_list.append(self.vocab_inverse[m.group().encode("utf-8")])
 
-    mini_chunk_size = 4096  # Read ahead by 4k bytes at a time
+        remainder = text[last_idx:]
+        for word_match in re.finditer(PAT, remainder):
+            word = word_match.group()
+            bytes_tokens = [bytes([i]) for i in word.encode("utf-8")]
+            
+            while True:
+                pairs = [(i, (bytes_tokens[i], bytes_tokens[i+1]))
+                        for i in range(len(bytes_tokens)-1)]
+                min_pair = min(pairs, key=lambda x: self.merges_ranks.get(x[1], float("inf")), default=None)
+                if not min_pair or self.merges_ranks.get(min_pair[1], float("inf")) == float("inf"):
+                    break
 
-    for bi in range(1, len(chunk_boundaries) - 1):
-        initial_position = chunk_boundaries[bi]
-        file.seek(initial_position)  # Start at boundary guess
-        while True:
-            mini_chunk = file.read(mini_chunk_size)  # Read a mini chunk
+                idx, (start, end) = min_pair
+                bytes_tokens[idx] = start + end
+                bytes_tokens.pop(idx+1)
 
-            # If EOF, this boundary should be at the end of the file
-            if mini_chunk == b"":
-                chunk_boundaries[bi] = file_size
-                break
+            token_list.extend([self.vocab_inverse[i] for i in bytes_tokens])
 
-            # Find the special token in the mini chunk
-            found_at = mini_chunk.find(split_special_token)
-            if found_at != -1:
-                chunk_boundaries[bi] = initial_position + found_at
-                break
-            initial_position += mini_chunk_size
+        return token_list
+   
+    def encode_iterable(self, iterable: Iterable[str]) -> Iterator[int]:
+        for text in iterable:
+            token_ids = self.encode(text)
+            yield from token_ids
 
-    # Make sure all boundaries are unique, but might be fewer than desired_num_chunks
-    return sorted(set(chunk_boundaries))
-
-
-def word_to_bytes(word: str) -> tuple[bytes, ...]:
-    return tuple(bytes([i]) for i in word.encode(encoding="utf-8"))
-
-
-PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
-
-
-def process_chunk(input_path, start, end, special_tokens):
-    word_counter = Counter()
-
-    with open(input_path, "rb") as f:
-        f.seek(start)
-        chunk_data = f.read(end - start).decode("utf-8")
-
-    # Removing special tokens before pre-tokenization
-    split_chunks = re.split("|".join(map(re.escape, special_tokens)), chunk_data)
-    for split_chunk in split_chunks:
-        for word_match in re.finditer(PAT, split_chunk):
-            word = word_match.group(0)
-            word_counter[word_to_bytes(word)] += 1
-
-    return word_counter
-
-
-def train_bpe(
-    input_path: str, vocab_size: int, special_tokens: list[str]
-) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
-    # step 1: Vocabulary initialization
-    vocab: dict[int, bytes] = {i: bytes([i]) for i in range(256)}
-    for token in special_tokens:
-        b_token = token.encode(encoding="utf-8")
-        if b_token not in vocab.values():
-            vocab[len(vocab)] = b_token
-
-    # step 2: Pre-tokenization
-    with open(input_path, "rb") as f:
-        chunk_boundaries = find_chunk_boundaries(f, 4, split_special_token=b"<|endoftext|>")
-
-    chunks = [(chunk_boundaries[i], chunk_boundaries[i + 1]) for i in range(len(chunk_boundaries) - 1)]
-    word_counter = Counter()
-    with ProcessPoolExecutor() as executor:
-        futures = [executor.submit(process_chunk, input_path, start, end, special_tokens) for start, end in chunks]
-        for future in futures:
-            word_counter.update(future.result())
-
-    # step 3: Compute BPE merges
-    merges = []
-    iter_num = vocab_size - len(vocab)
-
-    for _ in tqdm(range(iter_num), desc="Compute BPE merges"):
-        pair_counter: dict[tuple[bytes, bytes], int] = Counter()
-
-        # compute max
-        for bytes_array, cnt in word_counter.items():
-            for start, end in zip(bytes_array[:-1], bytes_array[1:]):
-                pair_counter[(start, end)] += cnt
-
-        max_num = max(pair_counter.values())
-        max_pair: tuple[bytes, bytes] = max([k for k, v in pair_counter.items() if v == max_num])
-
-        a, b = max_pair
-        new_bytes = a + b
-
-        # change word_counter
-        changes = []
-        for bytes_array, cnt in word_counter.items():
-            matchs = [i for i in range(len(bytes_array) - 1) if bytes_array[i : i + 2] == max_pair]
-            if matchs:
-                new_bytes_array = []
-                i = 0
-                while i < len(bytes_array):
-                    if i in matchs:
-                        new_bytes_array.append(new_bytes)
-                        i += 2
-                    else:
-                        new_bytes_array.append(bytes_array[i])
-                        i += 1
-                new_bytes_array = tuple(new_bytes_array)
-                changes.append((bytes_array, new_bytes_array, cnt))
-
-        for bytes_array, new_bytes_array, cnt in changes:
-            word_counter[new_bytes_array] = word_counter.get(new_bytes_array, 0) + cnt
-            del word_counter[bytes_array]
-
-        vocab[len(vocab)] = new_bytes
-        merges.append(max_pair)
-
-    return vocab, merges
-
-if __name__ == "__main__":
-    from time import time
-    st = time()
-    vocab, merges = train_bpe("data/TinyStoriesV2-GPT4-valid.txt", 1000, special_tokens=["<|endoftext|>"])
-    end = time()
-    print(end-st)
+    def decode(self, ids: list[int]) -> str:
+        bytes_list = [self.vocab[i] for i in ids]
+        bytes_text: bytes = b''
+        for bytes_i in bytes_list:
+            bytes_text += bytes_i
+        return bytes_text.decode("utf-8", errors="replace")
--- tests/adapters.py
+++ tests/adapters.py
@@ -559,7 +559,8 @@ def get_tokenizer(
     Returns:
         A BPE tokenizer that uses the provided vocab, merges, and special tokens.
     """
-    raise NotImplementedError
+    from cs336_basics.tokenizer import Tokenizer
+    return Tokenizer(vocab, merges, special_tokens)
 
 
 def run_train_bpe(
@@ -589,6 +590,6 @@ def run_train_bpe(
                 representing that <token1> was merged with <token2>.
                 Merges are ordered by order of creation.
     """
-    from cs336_basics.tokenizer import train_bpe
+    from cs336_basics.train_bpe import train_bpe
 
     return train_bpe(input_path, vocab_size, special_tokens)
